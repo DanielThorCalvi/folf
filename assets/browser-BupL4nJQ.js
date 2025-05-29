1d900aaa026c390723e2100daab6c8d72de4a89a
Daníel Þór Calvi <daniel.thor.calvi@advania.is>
2025-05-29 21:03:17 +0000

    Updates

---
+++ assets/browser-BupL4nJQ.js
@@ -0,0 +1,1 @@
+import{a4 as e}from"./_plugin-vue_export-helper-G6W3skjR.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
